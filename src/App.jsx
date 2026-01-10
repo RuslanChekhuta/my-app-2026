7ec7482417f9252a7269ee396b8c0ae4e8a40329
RuslanChekhuta <ruslan.gpt2025@gmail.com>
2026-01-10 20:39:08 +0100

    шаблон

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import "./App.css";
-import LegacyTodo from "./components/LegacyTodo";
-import WelcomeClass from "./components/WelcomeClass";
-
 function App() {
-  return (
-    <>
-      <WelcomeClass name="Студент" />
-      <hr />
-      <LegacyTodo />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
